50eeb7071db9bd56634f1d06af85bf5355c39135
Syed MH <hassan@8thloop.com>
2024-11-10 08:05:39 +0500

    feat: added toast notifier functionality

---
+++ frontend/src/utils/toast.ts
@@ -0,0 +1,15 @@
+import toast from "react-hot-toast";
+
+export const showSuccessToast = (message: string) => {
+  toast.success(message);
+};
+
+export const showErrorToast = (message: string) => {
+  toast.error(message);
+};
+
+export const showInfoToast = (message: string) => {
+  toast(message, {
+    icon: "ℹ️",
+  });
+};
